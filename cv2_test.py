9fa707de28897c59b6108d8f9a7bad1a34b37dbd
zhouc <1022590068@qq.com>
2022-06-22 20:59:59 +0800

    fix: filter fly point

--- cv2_test.py
+++ cv2_test.py
@@ -9,16 +9,80 @@ from typing import Dict, List
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
-def fit_line(lines: np.ndarray, pos_offset=100, angle_offset=10, thickness=2, gap_filter=50):
+def get_line_point(lines: list, offset: tuple) -> list:
+    """
+    获取线段两端点坐标
+    Args:
+        lines (list): 霍夫直线结果线段
+        offset (tuple): 线段偏移比值，线段偏移值，线段偏移角度，当角度偏移为90度时，偏移值为x坐标
+    Returns:
+        list: 线段两端点坐标 [point1, point2]
+    """
+    k, b, angle = offset
+    x1, y1, x2, y2 = lines[0]
+    min_x, max_x = (x1, x2) if x1 < x2 else (x2, x1)
+    min_y, max_y = (y1, y2) if y1 < y2 else (y2, y1)
+    for line in lines[1:]:
+        x1, y1, x2, y2 = line
+        min_x = min(min_x, x1, x2)
+        max_x = max(max_x, x1, x2)
+        min_y = min(min_y, y1, y2)
+        max_y = max(max_y, y1, y2)
+    if angle not in (0, 90):
+        min_y = min_x * k + b
+        max_y = max_y * k + b
+    return [(min_x, min_y), (max_x, max_y)]
+
+
+def get_pos_length(point1: tuple, point2: tuple):
+    """
+    获取两点之间长度
+    Args:
+        point1 (tuple): 点1坐标
+        point2 (tuple): 点2坐标
+    Returns:
+        float: 两点之间长度
+    """
+    line_length = math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+    return line_length
+
+
+def sort_group(lines: list, angle: float):
+    """
+    对线段组排序
+    Args:
+        lines (list): 线段组
+        angle (float): 线段偏移角度
+    Returns:
+        list: 排序后的线段组
+    """
+    if not lines:
+        return lines
+    if angle == 90:
+        lines = list(sorted(lines, key=lambda x: x[0][1]))
+    else:
+        lines = list(sorted(lines, key=lambda x: x[0][0]))
+    return lines
+
+
+def split_group(start_x, start_y, end_x, end_y, line, result, gap_filter):
+    """
+    拆分同一线段组
+    """
+    pass
+
+
+def fit_line(lines: List[np.ndarray], pos_offset=150, angle_offset=10, gap_filter=150, line_gap=500, line_lenth=100):
     """
     拟合直线
     Args:
         lines (list): 霍夫直线结果线段
         pos_offset (int): 线段允许偏移量
         angle_offset (float): 线段允许偏移角度
-        thickness (int): 线宽
         gap_filter (int): 缝隙大小
+        point_gap (int): 线与线之间的间隙
     """
     # 已归类的线段
     line_group: List[List] = []
@@ -26,97 +90,110 @@ def fit_line(lines: np.ndarray, pos_offset=100, angle_offset=10, thickness=2, ga
     line_offset: List[tuple] = []
     # 最终线段
     result: List = []
-    # 未能归类的线段
-    line_unfind: List = []
 
-    test_line = []
-
+    # 对线段进行归类
     for line in lines:
         for x1, y1, x2, y2 in line:
-            if 600 < y1 < 800 and 600 < y2 < 800 and 500 < x1 < 1200 and 500 < x2 < 1200 and x1 != x2:
-                if (y1 - y2) / (x1 - x2) < 0:
-                    test_line.append((x1, y1, x2, y2))
-
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            # print(x1, y1, x2, y2, line)
-            # 水平线段
             if x1 == x2:
-                angle = 90
-                a = 0
-                b = x1
+                # 水平线段
+                angle, a, b = 90, 0, x1
             elif y1 == y2:
-                angle = 0
-                a = 0
-                b = y1
+                # 垂直线段
+                angle, a, b = 0, 0, y1
             else:
+                # 倾斜线段
                 a = (y2 - y1) / (x2 - x1)
                 b = y1 - a * x1
                 angle = np.arctan(a) * 57.29577
             is_find_same_line = False
             for index, offset in enumerate(line_offset):
                 if abs(angle - offset[2]) < angle_offset and abs(b - offset[1]) < pos_offset:
-                    if (x1, y1, x2, y2) in test_line:
-                        print(angle, b, offset)
                     line_group[index].append(line)
                     is_find_same_line = True
                     break
             if not is_find_same_line:
                 line_group.append([line])
                 line_offset.append((a, b, angle))
-            if (x1, y1, x2, y2) in test_line:
-                print(is_find_same_line, (a, b, angle), index)
-    # print('--------------')
 
-    # print('--------------')
+    # 各组线段求端值
     for index, group_item in enumerate(line_group):
-        # print('print group and offset-------------------')
-        # print(group_item)
-        # print(line_offset[index])
         min_x, max_x = 0, 0
-        # if len(group_item) < 10:
-        #     continue
         a, b, angle = line_offset[index]
         min_y, max_y = 0, 0
-        for line in group_item:
-            for x1, y1, x2, y2 in line:
-                if angle == 0:
-                    if min_x == 0:
-                        min_x = min(x1, x2)
-                    else:
-                        min_x = min(min_x, min(x1, x2))
-                    max_x = max(max_x, max(x1, x2))
-                    min_y = y1
-                    max_y = y1
-                elif angle == 90:
-                    min_x = max_x = x1
-                    if min_y == 0:
-                        min_y = min(y1, y2)
-                    else:
-                        min_y = min(min_y, min(y1, y2))
-                    max_y = max(max_y, max(y1, y2))
+
+        # 仅考虑x升序的情况
+        group_item = sort_group(group_item, angle)
+        start_x, start_y, end_x, end_y = group_item[0][0]
+        # start_x, end_x = min(start_x, end_x), max(start_x, end_x)
+        for item in group_item[1:]:
+            x1, y1, x2, y2 = item[0]
+
+            if angle == 0:
+                new_min_x, new_max_x = (x1, x2) if x1 < x2 else (x2, x1)
+                if new_min_x > end_x and abs(new_min_x - end_x) > gap_filter:
+                    if get_pos_length((start_x, start_y), (end_x, end_y)) > line_lenth:
+                        result.append(((start_x, end_x), (start_y, end_y)))
+                    start_x, end_x = new_min_x, new_max_x
                 else:
-                    if min_x == 0:
-                        min_x = min(x1, x2)
-                    else:
-                        min_x = min(min_x, min(x1, x2))
-                    max_x = max(max_x, max(x1, x2))
-                    max_y = int(a * max_x + b)
-                    min_y = int(a * min_x + b)
-        # a, b, angle = line_offset[index]
-        # max_y = int(a * max_x + b)
-        # min_y = int(a * min_x + b)
-        # print(min_x, max_x, min_y, max_y)
-        if index == 5:
-            print(min_x, max_x, min_y, max_y)
-        if abs(max_x-min_x) < gap_filter and abs(max_y - min_y) < gap_filter:
-            continue
-        result.append([(min_x, max_x), (min_y, max_y)])
-        # cv2.line(result, (min_x, min_y), (max_x, max_y), thickness)
-    # print(result[0])
+                    start_x = min(start_x, new_min_x)
+                    end_x = max(end_x, new_max_x)
+
+            elif angle == 90:
+                new_min_y, new_max_y = (y1, y2) if y1 < y2 else (y2, y1)
+                if new_min_y > end_y and abs(new_min_y - end_y) > gap_filter:
+                    if get_pos_length((start_x, start_y), (end_x, end_y)) > line_lenth:
+                        result.append(((start_x, end_x), (start_y, end_y)))
+                    start_y, end_y = new_min_y, new_max_y
+                else:
+                    start_y = min(start_y, new_min_y)
+                    end_y = max(end_y, new_max_y)
+
+            else:
+                new_min_x, new_min_y, new_max_x, new_max_y = (x1, y1, x2, y2) if x1 < x2 else (x2, y2, x1, y1)
+                if new_min_x > end_x and get_pos_length((end_x, end_y), (new_min_x, new_min_y)) > line_gap:
+                    if get_pos_length((start_x, start_y), (end_x, end_y)) > line_lenth:
+                        result.append(((start_x, end_x), (start_y, end_y)))
+                    start_x, start_y, end_x, end_y = new_min_x, new_min_y, new_max_x, new_max_y
+                else:
+                    start_x, start_y = (start_x, start_y) if start_x < new_min_x else (new_min_x, new_min_y)
+                    end_x, end_y = (end_x, end_y) if end_x > new_max_x else (new_max_x, new_max_y)
+        if get_pos_length((start_x, start_y), (end_x, end_y)) > line_lenth:
+            result.append(((start_x, end_x), (start_y, end_y)))
+
+
+        # # 旧版直线拟合方法
+        # for line in group_item:
+        #     for x1, y1, x2, y2 in line:
+        #         if angle == 0:
+        #             if min_x == 0:
+        #                 min_x = min(x1, x2)
+        #             else:
+        #                 min_x = min(min_x, min(x1, x2))
+        #             max_x = max(max_x, max(x1, x2))
+        #             min_y = y1
+        #             max_y = y1
+        #         elif angle == 90:
+        #             min_x = max_x = x1
+        #             if min_y == 0:
+        #                 min_y = min(y1, y2)
+        #             else:
+        #                 min_y = min(min_y, min(y1, y2))
+        #             max_y = max(max_y, max(y1, y2))
+        #         else:
+        #             if min_x == 0:
+        #                 min_x = min(x1, x2)
+        #             else:
+        #                 min_x = min(min_x, min(x1, x2))
+        #             max_x = max(max_x, max(x1, x2))
+        #             max_y = int(a * max_x + b)
+        #             min_y = int(a * min_x + b)
+        # if abs(max_x-min_x) < gap_filter and abs(max_y - min_y) < gap_filter:
+        #     continue
+        # result.append([(min_x, max_x), (min_y, max_y)])
+        # # cv2.line(result, (min_x, min_y), (max_x, max_y), thickness)
     return result
 
-file_name = '2'
+file_name = '6'
 
 img_dir = f"./{file_name}.jpg"
 save_dir = f"./{file_name}_result.jpg"
@@ -160,8 +237,12 @@ lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 20, np.array([]), minLineLength, 
 # print(lines)
 # print('----------------')
 
-# 设置范围
 plt.subplot(2, 2, 1)
+plt_img = imread(img_dir)
+plt.imshow(img)
+
+# 设置范围
+plt.subplot(2, 2, 2)
 plt.xlim(0, w)
 plt.ylim(0, h)
 
